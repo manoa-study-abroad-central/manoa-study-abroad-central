e157ba2c621964a59f4eb0b43dcc86faad280170
Karissa Tabadero <143388266+karissat2@users.noreply.github.com>
2023-11-19 00:46:21 -1000

    Redo Program Selection Page

--- app/imports/ui/pages/Programs.jsx
+++ app/imports/ui/pages/Programs.jsx
@@ -1,51 +1,62 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
-import SimpleSchema from 'simpl-schema';
-import { AutoForm, SelectField } from 'uniforms-bootstrap5';
-import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 
 // eslint-disable-next-line max-len
 const countries = ['Australia', 'Canada', 'China', 'Czech Republic', 'Denmark', 'Fiji', 'Finland', 'France', 'French Polynesia', 'Germany', 'Hong Kong', 'Indonesia', 'Italy', 'Japan', 'Korea', 'Malaysia', 'Morocco', 'Netherlands', 'New Zealand', 'Norway', 'Philippines', 'Singapore', 'Spain', 'Sweden', 'Switzerland', 'Taiwan', 'Thailand', 'United Kingdom'];
 const programs = ['Manoa International Exchange (MIX)', 'Study Abroad Center', 'National Student Exchange (NSE)'];
 
-const formSchema = new SimpleSchema({
-  countryRegion: {
-    type: String,
-    allowedValues: countries,
-    label: 'Country/Region',
-  },
-  program: {
-    type: String,
-    allowedValues: programs,
-  },
-});
-
-const bridge = new SimpleSchema2Bridge(formSchema);
-
 const Programs = () => {
 
-  const submit = (data) => {
-    console.log(data);
+  const [selected, setSelected] = React.useState('');
+  const changeSelectionOption = (event) => {
+    setSelected(event.target.value);
   };
+
+  let type = null;
+  let options = null;
+  let title = '';
+  if (selected === 'Select') {
+    type = null;
+  } else if (selected === 'Program') {
+    type = programs;
+    title = 'Select Program';
+  } else if (selected === 'Country/Region') {
+    type = countries;
+    title = 'Select Country/Region';
+  }
+
+  if (type) {
+    options = type.map((el) => <option key={el}>{el}</option>);
+  }
 
   return (
     <Container className="py-3">
       <Row className="justify-content-center">
         <Col xs={12} md={4}>
           <h2 className="text-center" style={{ fontFamily: 'Roboto', fontweight: '300px', fontSize: '35px' }}>Program Selection Page </h2>
-          <Card style={{ borderRadius: '40px', borderWidth: '3px', borderColor: '#000' }}>
+          <Card style={{ borderRadius: '40px', borderWidth: '3px', borderColor: '#000', width: '425px' }}>
             <Card.Body>
-              <h3 className="text-left" style={{ fontWeight: 'bold' }}>Program Selection</h3>
+              <h3 className="text-center" style={{ fontWeight: 'bold' }}>Program Selection</h3>
               <hr />
-              <AutoForm schema={bridge} onSubmit={submit}>
-                <SelectField name="countryRegion" placeholder="Select Country" />
-                <SelectField name="program" placeholder="Select Program" />
-                <div className="d-flex justify-content-center">
-                  <button type="submit" className="btn mt-2" style={{ backgroundColor: 'white', color: 'black', borderColor: 'black', borderWidth: '3px', fontWeight: 'bold' }}>
-                    Search
-                  </button>
-                </div>
-              </AutoForm>
+              <h4 style={{ paddingTop: '15px' }}>Program or Country/Region</h4>
+              <select style={{ width: '375px', height: '40px' }} onChange={changeSelectionOption}>
+                <option>Select</option>
+                <option>Program</option>
+                <option>Country/Region</option>
+              </select>
+              <div style={{ paddingBottom: '60px' }} />
+              <h4>{ title }</h4>
+              <select style={{ width: '375px', height: '40px' }}>
+                {
+                  options
+                }
+              </select>
+              <div style={{ paddingBottom: '40px' }} />
+              <div className="d-flex justify-content-center">
+                <button type="submit" className="btn mt-2" style={{ backgroundColor: 'white', color: 'black', borderColor: 'black', borderWidth: '3px', fontWeight: 'bold' }}>
+                  Search
+                </button>
+              </div>
             </Card.Body>
           </Card>
         </Col>
