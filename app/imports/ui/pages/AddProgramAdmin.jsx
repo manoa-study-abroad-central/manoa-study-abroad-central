cf9a763c55dfbe6c1ff056f4f422724ccfffd7c1
Junlang Chen <143589428+JunlangChenGT@users.noreply.github.com>
2023-12-06 10:12:13 -1000

    Update AddProgramAdmin.jsx
    deleted meteor import

--- app/imports/ui/pages/AddProgramAdmin.jsx
+++ app/imports/ui/pages/AddProgramAdmin.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import { AutoForm, ErrorsField, LongTextField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
-import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import SimpleSchema from 'simpl-schema';
 import { Programs } from '../../api/program/Program';
