f35cf14819aa66c7ae3e9a2d91c2f913933d6523
Yi <chrissieskods@gmail.com>
2023-12-10 15:15:39 -1000

    Update HomePageAdmin.jsx

--- app/imports/ui/pages/HomePageAdmin.jsx
+++ app/imports/ui/pages/HomePageAdmin.jsx
@@ -12,24 +12,6 @@ const HomePageAdmin = () => (
       </Col>
     </Row>
     <center><Image className="landing-image" src="../images/study-abroad-clipart.jpg" /></center>
-    <Row className="fluid">
-      <landing-description>Manoa Study Abroad Central is a digital platform dedicated to simplifying the process of choosing a study abroad or exchange program at UH Manoa.
-        Since the programs listed below offer different types of experiences, it can be hard for students to determine which study abroad program is best for their
-        wants and needs. This site aims to clarify program details and provide clear comparisons to assist students in making informed decisions about their
-        international educational experiences.
-      </landing-description>
-    </Row>
-    <Row>
-      <Col>
-        <Image className="logos" src="../images/mix-logo.png" />
-      </Col>
-      <Col>
-        <Image className="logos" src="../images/nse-logo.png" />
-      </Col>
-      <Col>
-        <Image className="logos" src="../images/sac-logo.png" />
-      </Col>
-    </Row>
     <Row className="align-middle text-center">
       <Col className="d-flex flex-column justify-content-center">
         <Row className="py-3">
